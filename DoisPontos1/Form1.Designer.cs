211392966e6038fc194499b7b6245507fc45734e
Agrupamento de Escolas Maximinos <aemaximinosweb@gmail.com>
2019-12-03 16:39:32 +0000

    Adicionar arquivos de projeto.

---
+++ DoisPontos1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace DoisPontos1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.y3 = new System.Windows.Forms.TextBox();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.z1 = new System.Windows.Forms.TextBox();
+            this.z2 = new System.Windows.Forms.TextBox();
+            this.z3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(289, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Distancia entre pontos";
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(195, 149);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(130, 20);
+            this.x1.TabIndex = 1;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(349, 149);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(130, 20);
+            this.y1.TabIndex = 2;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(195, 205);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(130, 20);
+            this.x2.TabIndex = 3;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(349, 205);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(130, 20);
+            this.y2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ponto 1";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(99, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ponto 2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(360, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 66);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(99, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ponto 3";
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(349, 256);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(130, 20);
+            this.y3.TabIndex = 9;
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(195, 256);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(130, 20);
+            this.x3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(395, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 22);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(245, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 22);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "X";
+            // 
+            // z1
+            // 
+            this.z1.Location = new System.Drawing.Point(516, 149);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(130, 20);
+            this.z1.TabIndex = 14;
+            // 
+            // z2
+            // 
+            this.z2.Location = new System.Drawing.Point(516, 205);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(130, 20);
+            this.z2.TabIndex = 15;
+            // 
+            // z3
+            // 
+            this.z3.Location = new System.Drawing.Point(516, 257);
+            this.z3.Name = "z3";
+            this.z3.Size = new System.Drawing.Size(130, 20);
+            this.z3.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(564, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 22);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.z3);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox y3;
+        private System.Windows.Forms.TextBox x3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox z1;
+        private System.Windows.Forms.TextBox z2;
+        private System.Windows.Forms.TextBox z3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DoisPontos1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoisPontos1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+            T.P1.X = Convert.ToDouble(x1.Text);
+        }
+    }
+}
